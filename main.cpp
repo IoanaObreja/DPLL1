3820522f54a0cacd9c29a96b1ea1f94cb46f06ca
IoanaObreja <73855111+IoanaObreja@users.noreply.github.com>
2021-12-18 13:34:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ map<int, int> var_app;
 
 void read(int &variables, int &clauses, list< list<int> > &formula) {
 
-    // store the formula in a list of lists
-    // read e
+    /// store the formula in a list of lists
+    /// increment number of apparitions for each literal read
     int x;
     f>>variables>>clauses;
     for(int i=0;i<clauses;i++) {
@@ -31,6 +31,8 @@ void read(int &variables, int &clauses, list< list<int> > &formula) {
 
 int find_unit_clause(list< list<int> > formula) {
 
+    /// a clause is a unit clause if the size is 2, a literal and 0
+
     for(auto& clause: formula)
         if(clause.size() == 2)
             return clause.front();
@@ -38,6 +40,10 @@ int find_unit_clause(list< list<int> > formula) {
 }
 
 int find_pure_literal(list<list<int>> formula) {
+
+    /// search in var_app for
+    /// a literal that appears at least once
+    /// and the negated form never
 
     for(int i=1;i<=variables;i++) {
         if(var_app[i] > 0 && var_app[(-1)*i] == 0)
@@ -55,6 +61,7 @@ list<list<int>> unit_propagate(list<list<int>> formula, int literal) {
     /// search for the literal in each clause
     /// delete clauses that contain the literal
     /// delete the negated literal
+    /// decrement number of apparitions
 
     for(auto& clause: formula) {
         for(auto& lit: clause) {
@@ -63,7 +70,6 @@ list<list<int>> unit_propagate(list<list<int>> formula, int literal) {
                 clause.remove(lit);
             }
             else if(lit == literal){
-                // clause.erase(clause.begin(), clause.end());
                 for(auto& lit2: clause) {
                     var_app[lit2]--;
                     clause.remove(lit2);
@@ -78,10 +84,11 @@ list<list<int>> unit_propagate(list<list<int>> formula, int literal) {
 list<list<int>> pure_literal(list<list<int>> formula, int literal) {
 
     /// delete the clauses that contain a pure literal
+    /// decrement apparitions
+
     for(auto& clause: formula) {
         for(auto& lit: clause) {
             if(lit == literal) {
-                // clause.erase(clause.begin(), clause.end());
                 for(auto& lit2: clause) {
                     var_app[lit2]--;
                     clause.remove(lit2);
@@ -95,9 +102,7 @@ list<list<int>> pure_literal(list<list<int>> formula, int literal) {
 
 void print(list<list<int>> formula) {
 
-    //int i=1;
     for(auto& clause: formula) {
-        //g<<i++<<". ";
         for(auto& lit: clause)
             g<<lit<<' ';
         g<<'\n';
@@ -107,6 +112,7 @@ void print(list<list<int>> formula) {
 
 bool empty_clause(list<list <int>> formula) {
 
+    /// if the formula contains only 0
     for(auto& clause: formula)
         if(clause.size() == 1)
             return true;
@@ -115,6 +121,10 @@ bool empty_clause(list<list <int>> formula) {
 
 
 void print_assignment(list<int> assignment) {
+
+    /// print assignment
+    /// if some variables were not assigned, they are given value true
+    /// v is a frequency array
 
     int v[variables+1];
     for(int i=1;i<=variables;i++)
@@ -143,6 +153,7 @@ bool empty_formula(list<list<int>> formula) {
 }
 
 int find_first_lit (list<list<int>> formula) {
+
     for(auto& clause: formula)
         for(auto& lit: clause)
             if(lit!=0)
@@ -165,6 +176,7 @@ bool dpll(list<list <int>> formula, list<int> assignment) {
 
 
     int literal =0;
+    /// unit propagation as long as possible
     while(literal = find_unit_clause(formula)) {
         assignment.push_back(literal);
         formula = unit_propagate(formula, literal);
@@ -174,6 +186,7 @@ bool dpll(list<list <int>> formula, list<int> assignment) {
             return false;
         }
     }
+    /// pure literal as long as possible
     while(literal = find_pure_literal(formula)) {
         assignment.push_back(literal);
         formula = pure_literal(formula, literal);
@@ -190,18 +203,20 @@ bool dpll(list<list <int>> formula, list<int> assignment) {
         return true;
     }
 
-    literal = find_first_lit(formula);
-    //literal = find_most_popular_lit(formula);
-
+    /// variable decision
+    //literal = find_first_lit(formula);
+    literal = find_most_popular_lit(formula);
+    /// copy for var_app
     map<int,int> var_app_copy;
 
     for(int i=1;i<=variables;i++) {
         var_app_copy[i] = var_app[i];
         var_app_copy[(-1)*i] = var_app[(-1)*i];
     }
-
+    /// copy for formula + clause containing the literal for split
     list<list<int>> formula1;
     formula1 = formula;
+    /// split 1
     formula1.push_back({literal, 0});
     //g<<"Split on "<<literal<<'\n';
     if(dpll(formula1, assignment))
@@ -211,9 +226,10 @@ bool dpll(list<list <int>> formula, list<int> assignment) {
         var_app[i] = var_app_copy[i];
         var_app[(-1)*i] = var_app_copy[(-1)*i];
     }
-
+    /// copy for formula + clause containing the negated literal for split
     list<list<int>> formula2;
     formula2 = formula;
+    /// split 2
     formula2.push_back({(-1)*literal, 0});
     //g<<"Split on "<<(-1)*literal<<'\n';
     if(dpll(formula2, assignment))
@@ -235,7 +251,7 @@ int main()
     //print(formula);
 
     auto start = high_resolution_clock::now();
-    if(!dpll(formula, {})) g<<"UNSAT";
+    if(!dpll(formula, {})) g<<"UNSAT\n";
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     g << "\ntime: " << duration.count() << endl;
